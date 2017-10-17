fb9ffd931389227267d7b28d9570df902affa6ec
Denis4911 <deniskamotskiy@ro.ru>
2017-10-17 22:42:15 +0500

    11

--- js/jcarousel.responsive.js
+++ js/jcarousel.responsive.js
@@ -1,18 +1,16 @@
 (function($) {
     $(function() {
-        var jcarousel = $('#our-partners .jcarousel');
+        var jcarousel = $('.jcarousel');
 
         jcarousel
             .on('jcarousel:reload jcarousel:create', function () {
                 var carousel = $(this),
                     width = carousel.innerWidth();
 
-                if (width <= 730) {
+                if (width >= 600) {
                     width = width / 3;
-                } else if (width <= 950) {
-                    width = width / 4;
-                } else if (width >= 950) {
-                    width = width / 5;
+                } else if (width >= 350) {
+                    width = width / 2;
                 }
 
                 carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
@@ -21,121 +19,17 @@
                 wrap: 'circular'
             });
 
-        $('#our-partners .jcarousel-control-prev')
+        $('.jcarousel-control-prev')
             .jcarouselControl({
                 target: '-=1'
             });
 
-        $('#our-partners .jcarousel-control-next')
+        $('.jcarousel-control-next')
             .jcarouselControl({
                 target: '+=1'
             });
 
-        $('#our-partners .jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .on('click', function(e) {
-                e.preventDefault();
-            })
-            .jcarouselPagination({
-                perPage: 1,
-                item: function(page) {
-                    return '<a href="#' + page + '">' + page + '</a>';
-                }
-            });
-    });
-})(jQuery);
-// ==========================================================================================================
-(function($) {
-    $(function() {
-        var jcarousel = $('#adaptiv-slider-menu .jcarousel');
-
-        jcarousel
-            .on('jcarousel:reload jcarousel:create', function () {
-                var carousel = $(this),
-                    width = carousel.innerWidth();
-
-                if (width >= 730) {
-                    width = width / 4;
-                } else if (width >= 50) {
-                    width = width / 3;
-                } else if (width >= 950) {
-                    width = width / 5;
-                }
-
-                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-            })
-            .jcarousel({
-                wrap: 'circular'
-            });
-
-        $('#adaptiv-slider-menu .jcarousel-control-prev')
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('#adaptiv-slider-menu .jcarousel-control-next')
-            .jcarouselControl({
-                target: '+=1'
-            });
-
-        $('#adaptiv-slider-menu .jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .on('click', function(e) {
-                e.preventDefault();
-            })
-            .jcarouselPagination({
-                perPage: 1,
-                item: function(page) {
-                    return '<a href="#' + page + '">' + page + '</a>';
-                }
-            });
-    });
-})(jQuery);
-// ==========================================================================================================
-(function($) {
-    $(function() {
-        var jcarousel = $('#vproekte-main-block .col-md-8 .jcarousel');
-
-        jcarousel
-            .on('jcarousel:reload jcarousel:create', function () {
-                var carousel = $(this),
-                    width = carousel.innerWidth();
-
-                if (width >= 500) {
-                    width = width / 4;
-                } else if (width >= 50) {
-                    width = width / 3;
-                } else if (width >= 950) {
-                    width = width / 5;
-                }
-
-                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-            })
-            .jcarousel({
-                wrap: 'circular'
-            });
-
-        $('#vproekte-main-block .col-md-8 .jcarousel-control-prev')
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('#vproekte-main-block .col-md-8 .jcarousel-control-next')
-            .jcarouselControl({
-                target: '+=1'
-            });
-
-        $('#adaptiv-slider-menu .jcarousel-pagination')
+        $('.jcarousel-pagination')
             .on('jcarouselpagination:active', 'a', function() {
                 $(this).addClass('active');
             })
